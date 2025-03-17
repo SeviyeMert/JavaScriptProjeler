55b4aefb44fe0c1721076f9eb25c38eb7ddf5085
SeviyeMert <seviyemert99@gmail.com>
2025-03-17 03:45:38 +0300

    17mart3

--- BasitHesapMakinesi/script.js
+++ BasitHesapMakinesi/script.js
@@ -3,12 +3,12 @@ function appendValue(value) {
     display.value += value;
   }
   
-  function clearDisplay() {
-    document.getElementById('display').value = '';
-  }
-  
   function calculate() {
     let display = document.getElementById('display');
-
+    
     display.value = eval(display.value);
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
   }
