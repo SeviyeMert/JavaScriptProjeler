235219c8f10e519a40ea296a84d6f117039b308c
SeviyeMert <seviyemert99@gmail.com>
2025-03-18 15:23:20 +0300

    18mart3

---
+++ ToDoListProjesi/script.js
@@ -0,0 +1,8 @@
+//Tüm elemanları seç
+
+let form = document.querySelector("#todoAddForm");
+let addInput = document.querySelector("#addInput");
+let todoList = document.querySelector(".list-group");
+let firstCardBody = document.querySelectorAll(".card-body")[0];
+let secondCardBody = document.querySelectorAll(".card-body")[1];
+let clearButton = document.querySelector("#clearButton");
