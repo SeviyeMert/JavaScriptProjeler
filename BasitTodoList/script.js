1eb1e16e7e584a3332ce4d44244e57df8eaecf11
SeviyeMert <seviyemert99@gmail.com>
2025-04-08 13:05:02 +0300

    8nisan2

--- BasitTodoList/script.js
+++ BasitTodoList/script.js
@@ -4,58 +4,61 @@ window.onload = function() {
   };
 function gorevEkle() {
   const input = document.getElementById("gorevInput");
-  const gorev = input.value.trim();
+  const metin = input.value.trim();
 
-  if (gorev === "") {
+  if (metin === "") {
     alert("Lütfen bir görev giriniz.");
     return;
   }
 
-  listeyeEkle(gorev, false);
+  listeyeEkle(metin, false);
   input.value = "";
   gorevleriKaydet();
 }
 
 
 
-function listeyeEkle(gorev, tamamlandi) {
+function listeyeEkle(metin, tamamlandi) {
     const li = document.createElement("li");
-    li.textContent = gorev;
-    document.getElementById("görevListesi").appendChild(li);
+    li.textContent = metin;
+    
+    if (tamamlandi) {
+        li.classList.add("tamamlandi");
+    }
 
-  if (tamamlandi) {
-    li.classList.add("tamamlandi");
-  }
+    const silButon = document.createElement("button");
+    silButon.textContent = "X";
+    silButon.style.backgroundColor = "white";
+    silButon.style.border = "none";
+    silButon.style.marginLeft = "20px";
+    silButon.onclick = function (e) {
+        li.remove();
+        e.stopPropagation();
+        gorevleriKaydet();
+    };
+    
+    li.appendChild(silButon);
+    
+    li.onclick = function () {
+        li.classList.toggle("tamamlandi");
+        gorevleriKaydet();
+    };
+    
+    document.getElementById("gorevListesi").appendChild(li);
 }
 
 
-const silButon = document.createElement("button");
-silButon.textContent = "X";
-silButon.style.backgroundColor = "white";
-silButon.style.border = "none";
-silButon.style.marginLeft = "20px";
-silButon.onclick = function (e) {
-  li.remove();
-  e.stopPropagation();
-  gorevleriKaydet();
-};
 
-li.appendChild(silButon);
-
-li.onclick = function () {
-  li.classList.toggle("tamamlandi");
-  gorevleriKaydet();
-};
 
 function gorevleriKaydet() {
-    const liste = document.querySelectorAll("#görevListesi li");
+    const liste = document.querySelectorAll("#gorevListesi li");
     const gorevler = [];
 
     liste.forEach(li => {
-        const gorev = li.childNodes[0].textContent.trim();
-        const tamamlandi = li.classList.contains("tamamlandi");
-        gorevler.push({gorev, tamamlandi});
-    })
+      const metin = li.childNodes[0].textContent.trim();
+      const tamamlandi = li.classList.contains("tamamlandi");
+      gorevler.push({ metin, tamamlandi });
+    });
 
     localStorage.setItem("gorevler", JSON.stringify(gorevler));
 }
