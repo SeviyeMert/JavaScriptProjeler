e0f518957ccb52a0353e15b435e0247588832bc0
SeviyeMert <seviyemert99@gmail.com>
2025-04-08 21:41:59 +0300

    sayac2devamm

--- BasitHesapMakinesi/script.js
+++ BasitHesapMakinesi/script.js
@@ -1,14 +1,14 @@
+let display = document.getElementById("display");
+
+
 function appendValue(deger) {
-    let display = document.getElementById("display");
-    display.value += deger;
-  }
+  display.value += deger;
+}
   
   function calculate() {
-    let display = document.getElementById('display');
-    
     display.value = eval(display.value);
   }
   
-  function clearDisplay() {
-    document.getElementById('display').value = '';
+  function clear() {
+    display.value = '';
   }
--- Sayac2/script.js
+++ Sayac2/script.js
@@ -2,8 +2,17 @@ const deger = document.getElementById("sayi");
 const art = document.getElementById("arti");
 const azalt = document.getElementById("eksi");
 
+function minus(){
+    if (Number(deger.textContent)<= 0) {
+        azalt.disabled = true;
+    } else {
+        azalt.disabled = false;
+    }
+}
+
 function arttir() {
     deger.textContent = Number(deger.textContent) + 1;
+    minus();
 }
 
 art.onclick = arttir;
@@ -11,9 +20,12 @@ art.onclick = arttir;
 
 function azalttir() {
     deger.textContent = Number(deger.textContent) - 1;
+    minus();
 }
 
 azalt.onclick = azalttir;
+
+
 
 
 
