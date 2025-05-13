207bf40f6c50d14428568d74a018dd910c6d67c8
SeviyeMert <seviyemert99@gmail.com>
2025-05-13 23:45:32 +0300

    rastgele

--- RastgeleSifreUretici/script.js
+++ RastgeleSifreUretici/script.js
@@ -1,3 +1,13 @@
 const coptbtn = document.getElementById("copybtn");
 const uretbtn = document.getElementById("uretbtn");
 const password = document.getElementById("password");
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}<>?/"";
+
+uretbtn.addEventListener("click", () => {
+    const length = parseInt(password.value);
+    if ( length < 8 || length > 16){
+        alert("Şifre uzunluğu 8 ile 16 arasında olmalı.");
+        return;
+    }
+})
